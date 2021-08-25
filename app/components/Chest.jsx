6fed07f84275c00d91d7dafab4600a9ba860fe24
arnaud <arnaud.boidard@gmail.com>
2021-08-25 18:46:03 +0200

    ajout bouton scroll to top

--- app/components/Chest.jsx
+++ app/components/Chest.jsx
@@ -3,32 +3,42 @@ import { useItems } from "../hooks/items"
 import Item from "./Item"
 import { Loader } from "./Loader"
 import SearchUser from "./SearchUser"
+import { scrollToTop } from "../utils/window"
 
 function Chest(props) {
 
     const { items, loading, limitReach, fetchItems } = useItems()
     const [page, setPage] = useState(0)
+    const [buttonTopVisible, setButtonTopVisible] = useState(false)
     const [changePage, setChangePage] = useState(true)
     const itemsList = []
 
     const handleScroll = useCallback((e) => {
-        const bottom = document.documentElement.scrollHeight - document.documentElement.scrollTop === document.documentElement.clientHeight;
+        let scrollTop = document.documentElement.scrollTop
+        const bottom = document.documentElement.scrollHeight - scrollTop === document.documentElement.clientHeight;
 
+        // appel à la page suivante de l'api une fois arrivé en bas de l'écran
         if (bottom && !loading) {
             setChangePage(true)
         }
-    }, [loading, limitReach])
+
+        // on affiche le bouton pour revenir en haut de l'écran apres un scroll de 300
+        setButtonTopVisible(scrollTop > 300)
+
+    }, [loading])
 
     useEffect(() => {
+        //evenement de scroll pour le chargement infini + le bouton de retour au début de la page
         window.addEventListener("scroll", handleScroll, false);
     }, [])
 
     useEffect(() => {
         if (!changePage) return
+        if (limitReach) return
         fetchItems("12345678900", 12, page)
         setPage(prevState => prevState + 1)
         setChangePage(false)
-    }, [changePage])
+    }, [changePage, limitReach])
 
     for (const [index, value] of items.entries()) {
         itemsList.push(<Item key={index} itemInfo={value} />)
@@ -45,6 +55,7 @@ function Chest(props) {
                 {itemsList}
             </div>
             {loading ? <><Loader> Chargement...</Loader></> : ""}
+            {buttonTopVisible && <button className="fixed text-3xl rounded-md right-6 bottom-3 py-2 px-4 bg-red-700 text-white" onClick={scrollToTop}>🠕</button>}
         </>)
 }
 

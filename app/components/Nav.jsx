a0459d7e780313fa82a927a44a3d91461f7207f3
arnaud <arnaud.boidard@gmail.com>
2025-04-12 20:02:22 +0200

    new link upgrade

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -6,26 +6,28 @@ export default function Nav() {
     return (
         <nav className="flex items-center justify-between w-full max-w-4xl p-0 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 dark:bg-black bg-opacity-60 dark:text-gray-100">
             <div className="justify-center">
-                <Link href="/">
-                    <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Home</a>
+                <Link href="/" className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">
+                    Home
                 </Link>
-                <Link href="/chest">
-                    <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Chest</a>
+                <Link href="/chest" className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">
+                    Chest
                 </Link>
-                <Link href="/blogStatic">
-                    <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Blog</a>
+                <Link
+                    href="/blogStatic"
+                    className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">
+                    Blog
                 </Link>
-                <Link href="/support">
-                    <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Support</a>
+                <Link href="/support" className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">
+                    Support
                 </Link>
             </div>
             <div className="mt-2 mr-2">
-                <Link href="https://discord.gg/U28geccwM5" >
-                    <a target="_blank">
-                        <Image layout="fixed" src={discord} width={50} height={38} />
-                    </a>
+                <Link href="https://discord.gg/U28geccwM5" target="_blank">
+
+                    <Image layout="fixed" src={discord} width={50} height={38} />
+
                 </Link>
             </div>
         </nav>
-    )
+    );
 }
